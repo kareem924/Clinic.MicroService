9b3c2d123e1da63f431d5473da6acf1aca6a717f
Kareem <kareemmuhammad924@gmail.com>
2019-06-21 08:42:38 +0200

    Try to restructure the application from scratch

--- Security/Entities/RefreshToken.cs
+++ Security/Entities/RefreshToken.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 
-
 namespace Abstract.Security.Entities
 {
     public class RefreshToken
     {
-
         public int Id { get; set; }
         public DateTime ExpiresAt { get; set; }
         public string Token { get; set; }
