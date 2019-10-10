d2dd706dd98f952f31d78aef23a5dc335531add8
kmuhammad <kmuhammad@ASIM-MIS-WK04>
2019-10-10 11:08:18 +0200

    Add RestEase in Common

---
+++ Src/Common/Common.RestEase/CustomHttpClientHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Common.RestEase
+{
+    public class CustomHttpClientHandler : HttpMessageHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CustomHttpClientHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            return SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ Src/Common/Common.RestEase/RestApiAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.RestEase
+{
+    [AttributeUsage(AttributeTargets.Interface, Inherited = false, AllowMultiple = false)]
+    public sealed class RestApiAttribute : Attribute
+    {
+        public string BaseUrl { get; private set; }
+
+        public RestApiAttribute(string baseUrl)
+        {
+            BaseUrl = baseUrl;
+        }
+    }
+}
